1d905761124d651d5877b5d7c17169d6a2d2c1d8
mifye <mifyedev@gmail.com>
2023-01-06 18:56:47 -0600

    initial commit

---
+++ spotify-2/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <h1>This is a spotify 2.0 build</h1>
+
+      <main>
+        {/* Sidebar */}
+        {/* Center */}
+      </main>
+
+      <div>{/* Player */}</div>
+    </div>
+  )
+}
+/* hi */
+export default Home
